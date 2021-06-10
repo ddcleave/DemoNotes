d1ce1011cb8ab4298552b43bfe6f467f61b1e8d0
Sergey Turkov <ddcleave@gmail.com>
2021-06-10 12:38:24 +0300

    replaced working with json instead of form

--- auth_server/auth_server/api/dependencies/get_userdata_from_token.py
+++ auth_server/auth_server/api/dependencies/get_userdata_from_token.py
@@ -1,12 +1,12 @@
-from fastapi.param_functions import Depends
+from fastapi.param_functions import Body, Depends
 from auth_server.core.config import get_settings
-from fastapi import HTTPException, status, Form
+from fastapi import HTTPException, status
 from jose import JWTError, jwt
 from auth_server.api.dependencies.queries_to_redis import get_userdata
 
 
-def _get_userdata_from_token(scope: str):
-    def inner(token: str = Form(...)):
+def _get_subject_from_token(scope: str):
+    def inner(token: str = Body(..., embed=True)):
         credentials_exception = HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Could not validate credentials"
@@ -17,18 +17,18 @@ def _get_userdata_from_token(scope: str):
                                  algorithms=[get_settings().algorithm])
             if payload.get("scope") != scope:
                 raise credentials_exception
-            username = payload.get("sub")
-            if username is None:
+            subject = payload.get("sub")
+            if subject is None:
                 raise credentials_exception
         except JWTError:
             raise credentials_exception
-        return username
+        return subject
     return inner
 
 
-get_username_from_registration_token = _get_userdata_from_token("registration")
+get_username_from_registration_token = _get_subject_from_token("registration")
 
-get_username_from_restore_token = _get_userdata_from_token("restore")
+get_email_from_restore_token = _get_subject_from_token("restore")
 
 
 def get_userdata_from_registration_token(
